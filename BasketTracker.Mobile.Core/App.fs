ae80dedd6b462bae5a4c4efede8cd4dc27f32ac7
Kimserey Lam <kimserey.lam@gmail.com>
2016-07-30 11:35:15 +0100

    added Domain sample

--- BasketTracker.Mobile.Core/App.fs
+++ BasketTracker.Mobile.Core/App.fs
@@ -5,7 +5,7 @@ open Xamarin.Forms
 type App() = 
     inherit Application()
     let stack = StackLayout(VerticalOptions = LayoutOptions.Center)
-    let label = Label(XAlign = TextAlignment.Center, Text = "Welcome to F# Xamarin.Forms!")
+    let label = Label(XAlign = TextAlignment.Center, Text = "Test")
     do 
         stack.Children.Add(label)
         base.MainPage <- ContentPage(Content = stack)
---
+++ BasketTracker.Mobile.Core/Domain.fs
@@ -0,0 +1,91 @@
+﻿namespace BasketTracker.Mobile.Core
+
+open System
+
+module Domain =
+
+    type Stores = Stores of Store list
+        with
+            static member Sample = 
+                Stores [
+                    { Name = "Asda"
+                      Baskets = 
+                        [
+                            { Date = DateTime.Now
+                              Items = 
+                                [
+                                    { Name = "Apple"
+                                      Amount = 2.05m}
+                                    { Name = "Bread"
+                                      Amount = 3.5m}
+                                ] }
+                             
+                            { Date = DateTime.Now.AddDays(-1.)
+                              Items = 
+                                [
+                                  { Name = "Apple"
+                                    Amount = 2.05m}
+                                  { Name = "Bread"
+                                    Amount = 3.5m}
+                                ] }
+                        ]}
+
+                    { Name = "Tesco"
+                      Baskets =
+                        [
+                            { Date = DateTime.Now
+                              Items =
+                                [
+                                  { Name = "Apple"
+                                    Amount = 2.05m}
+                                  { Name = "Bread"
+                                    Amount = 3.5m}
+                                ] }
+                             
+                            { Date = DateTime.Now.AddDays(-1.)
+                              Items = 
+                                [
+                                  { Name = "Apple"
+                                    Amount = 2.05m}
+                                  { Name = "Bread"
+                                    Amount = 3.5m}
+                                ] }
+                        ]}
+
+                    { Name = "Waitrose"
+                      Baskets =
+                        [
+                            { Date = DateTime.Now
+                              Items = 
+                                [
+                                  { Name = "Apple"
+                                    Amount = 2.05m}
+                                  { Name = "Bread"
+                                    Amount = 3.5m}
+                                ] }
+                             
+                            { Date = DateTime.Now.AddDays(-1.)
+                              Items = 
+                                [
+                                  { Name = "Apple"
+                                    Amount = 2.05m}
+                                  { Name = "Bread"
+                                    Amount = 3.5m}
+                                ] }
+                        ]}
+                ]
+         
+    and Store = {
+        Name: string
+        Baskets: Basket list
+    } 
+
+    and Basket = {
+        Date: DateTime
+        Items: BasketItem list
+    }
+
+    and BasketItem = {
+        Name: string
+        Amount: decimal
+    }
