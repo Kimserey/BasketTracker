523dae33953219806a547416077ea55030cd13fc
Kimserey Lam <kimserey.lam@gmail.com>
2016-07-31 09:39:09 +0100

    Changed binding, improved structure

--- BasketTracker.Mobile.Core/App.fs
+++ BasketTracker.Mobile.Core/App.fs
@@ -3,55 +3,131 @@
 open System
 open System.Collections
 open System.Collections.Generic
+open System.ComponentModel
 open Domain
 open Xamarin.Forms
 
-type BasketView = {
-        Date: string
-        Amount: string
-    } with
-        static member FromDomain (basket: Basket) =
-            { Date = basket.Date.ToString("dd MMM yyyy")
-              Amount = (basket.Items |> List.sumBy (fun i -> i.Amount)).ToString("C2") }
+type ViewModelBase() =
+    let propertyChanging = new Event<PropertyChangingEventHandler, PropertyChangingEventArgs>()
+    let propertyChanged  = new Event<PropertyChangedEventHandler,  PropertyChangedEventArgs>()
+
+    interface INotifyPropertyChanged with
+        [<CLIEvent>]
+        member self.PropertyChanged = propertyChanged.Publish
+    
+    member self.PropertyChanging = propertyChanging.Publish
+
+    member self.OnPropertyChanging name =
+        propertyChanging.Trigger(self, new PropertyChangingEventArgs(name))
+
+    member self.OnPropertyChanged name =
+        propertyChanged.Trigger(self, new PropertyChangedEventArgs(name))
+
+
+type StoreDetailViewModel() = 
+    inherit ViewModelBase()
+
+    let mutable title = ""
+    let mutable baskets: BasketViewModel list = []
+
+    member self.Title 
+        with get() = title
+        and  set value = 
+            self.OnPropertyChanging "Title"
+            title <- value
+            self.OnPropertyChanged "Title"
+
+    member self.Baskets
+        with get() = baskets
+        and set value =
+            self.OnPropertyChanging "Baskets"
+            baskets <- value
+            self.OnPropertyChanged "Baskets"
+        
+and BasketViewModel = {
+    Date: string
+    Amount: string
+} with
+    static member FromDomain (basket: Basket) =
+        let sum = 
+            basket.Items 
+            |> List.sumBy (fun i -> i.Amount)
+
+        { Date   = basket.Date.ToString("dd MMM yyyy")
+          Amount = sum.ToString("C2") }
+
 
 [<AutoOpen>]
 module Store =
 
-    type BasketCell() =
-        inherit TextCell()
+    //type BasketEntry(store) =
+    //    inherit ContentPage(Title = store + " - basket")
+
+    //type BasketCell() =
+    //    inherit TextCell()
+
+    //    do
+    //        base.SetBinding(TextCell.TextProperty, "Date")
+    //        base.SetBinding(TextCell.DetailProperty, "Amount")
+
+    //type BasketList(baskets: BasketView list) =
+    //    inherit ListView(ItemTemplate = new DataTemplate(typeof<BasketCell>))
+
+    //    do
+    //        base.BindingContext <- box { Items = baskets }
+    //        base.SetBinding(ListView.ItemsSourceProperty, "Items")
+
+
+    //type StoreBasketListPage(defaultStore: Store) =
+    //    inherit ContentPage(Title = defaultStore.Name)
+
+    //    do
+    //        base.Content <- new BasketList(
+
+    //type StoreDetailPage(defaultStore: Store) as self =
+    //    inherit NavigationPage()
+
+    //    let item =
+    //        new ToolbarItem(
+    //            "New",
+    //            "plus",
+    //            fun () ->
+    //                self.PushAsync(new BasketEntry(defaultStore.Name))
+    //                |> Async.AwaitTask
+    //                |> Async.StartImmediate)
+
+    //    do
+    //        base.ToolbarItems.Add(item)
+
+    //    override self.OnBindingContextChanged() =
+    //        let store = self.BindingContext :?> Store
+    //        base.CurrentPage.BindingContext <- store
+
+    type StoreDetailPage(vm: StoreDetailViewModel) =
+        inherit ContentPage()
+
+        let basketList = new ListView(ItemTemplate = new DataTemplate(typeof<TextCell>))
 
         do
-            base.SetBinding(TextCell.TextProperty, "Date")
-            base.SetBinding(TextCell.DetailProperty, "Amount")
+            base.BindingContext <- vm
+            base.SetBinding(ContentPage.TitleProperty, "Title")
+            base.Content <- basketList
 
-    type BasketList(baskets: BasketView list) =
-        inherit ListView(ItemsSource = baskets, ItemTemplate = new DataTemplate(typeof<BasketCell>))
+            basketList
+                .ItemTemplate
+                .SetBinding(TextCell.TextProperty, "Date")
+            basketList
+                .ItemTemplate
+                .SetBinding(TextCell.DetailProperty, "Amount")
+            basketList
+                .SetBinding(ListView.ItemsSourceProperty, "Baskets")
 
-    type StoreDetailPage(name, baskets) as self =
-        inherit NavigationPage(new ContentPage(Title = name, Content = BasketList(baskets)))
 
-        let item =
-            new ToolbarItem(
-                "New",
-                "plus",
-                fun () ->
-                    self.DisplayAlert("Alert", "Something", "OK") 
-                    |> Async.AwaitTask 
-                    |> Async.Start)
-
-        do
-            base.ToolbarItems.Add(item)
-
-    type StoreMenuItemCell() =
-        inherit TextCell()
-
-        do
-            base.SetBinding(TextCell.TextProperty, "Name")
-
-    type StoreMasterPage(stores) =
+    type StoreMasterPage(stores, onSelect) as self =
         inherit ContentPage(Title = "Stores", Icon = FileImageSource.op_Implicit"hamburger")
 
-        let (Stores stores) = stores
+        let (Stores stores) = 
+            stores
 
         let title =
             let layout = 
@@ -74,10 +150,19 @@ module Store =
             layout
 
         let menu = 
-            new ListView(
-                ItemsSource = stores,
-                ItemTemplate = new DataTemplate(typeof<StoreMenuItemCell>),
-                VerticalOptions = LayoutOptions.FillAndExpand)
+            let list =
+                new ListView(
+                    ItemsSource = stores,
+                    ItemTemplate = new DataTemplate(typeof<TextCell>),
+                    VerticalOptions = LayoutOptions.FillAndExpand)
+
+            list
+                .ItemSelected
+                .Add(fun e -> onSelect(self, e.SelectedItem :?> Store))
+            list
+                .ItemTemplate
+                .SetBinding(TextCell.TextProperty, "Name")
+            list
         
         let layout =
             new StackLayout()
@@ -93,23 +178,19 @@ module Store =
 
             base.Content <- layout
 
-    type Root() =
+    type Root() as self =
         inherit MasterDetailPage()
 
         let stores =
             Stores.Sample
 
-        let store = 
-            let (Stores stores) = stores
-            stores.[0]
-
-        let baskets =
-            store.Baskets
-            |> List.map BasketView.FromDomain
-
         do
-            base.Master <- new StoreMasterPage(stores)
-            base.Detail <- new StoreDetailPage(store.Name, baskets)
+            let vm = new StoreDetailViewModel()
+            self.Detail <- new NavigationPage(StoreDetailPage(vm))
+            self.Master <- new StoreMasterPage(stores, (fun (page, selection) -> 
+                vm.Title <- selection.Name
+                vm.Baskets <- selection.Baskets |> List.map BasketViewModel.FromDomain
+                self.IsPresented <- false))
          
 type App() = 
     inherit Application()
