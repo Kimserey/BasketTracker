8fe0220fa7848fbc3b746eb5c3ba77e786621f33
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-08-03 23:00:38 +0100

    Changed to add context actions

--- BasketTracker.Mobile.Core/App2.fs
+++ BasketTracker.Mobile.Core/App2.fs
@@ -29,66 +29,16 @@ type StoreViewModel(storeId: int, connectionFactory: SQLiteConnectionFactory) =
         use conn = connectionFactory() 
         Storage.Store.Update storeId name conn
 
-
-type StoreMasterPage() as self =
-    inherit ContentPage(Title = "test", Icon = FileImageSource.op_Implicit "hamburger")
-
-    let title =
-        let masterTitle =
-            new Label(
-                Text = "",
-                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof<Label>),
-                HorizontalOptions = LayoutOptions.StartAndExpand,
-                VerticalOptions = LayoutOptions.CenterAndExpand
-            ) |> Label.SetBinding' Label.TextProperty "Name"
-
-        new StackLayout(
-            Padding = new Thickness(10.),
-            Orientation = StackOrientation.Horizontal)
-        |> StackLayout.AddChild (new Image(Source = FileImageSource.op_Implicit "shop"))
-        |> StackLayout.AddChild masterTitle
-
-    let buttonList =
-        new ListView(ItemsSource = [ "Edit"; "Delete" ], ItemTemplate = new DataTemplate(typeof<TextCell>))
-        |> ListView.SetTemplateBinding TextCell.TextProperty "."
-        
-    let layout =
-        new StackLayout()
-        |> StackLayout.AddChild title
-        |> StackLayout.AddChild buttonList
+type StorePage(storeId, connectionFactory: SQLiteConnectionFactory) =
+    inherit ContentPage()
+    
+    let vm =
+        new StoreViewModel(storeId, connectionFactory)
 
     do
         base.SetBinding(ContentPage.TitleProperty, "Name")
-        base.Content <- layout
-
-type StorePage(storeId: int, connectionFactory: SQLiteConnectionFactory) as self =
-    inherit MasterDetailPage(Title = "md test")
-
-    let vm = 
-        new StoreViewModel(storeId, connectionFactory)
-
-    let detail =
-        let page = new ContentPage(Title = "md test")
-        page.SetBinding(ContentPage.TitleProperty, "Name")
-        page.BindingContext <- vm
-        page
-
-    let master = 
-        let page = new StoreMasterPage()
-        page.BindingContext <- vm
-        page
-
-    let edit =
-        new ToolbarItem(
-            "Edit this store", 
-            "pencil", 
-            fun () -> self.IsPresented <- not self.IsPresented)
-
-    do
-        self.ToolbarItems.Add(edit)
-        self.SetBinding(MasterDetailPage.TitleProperty, "Name")
-        self.Detail <- detail
-        self.Master <- master
+        base.BindingContext <- vm
+    
 
 type AddStorePage(connectionFactory: SQLiteConnectionFactory) as self =
     inherit ContentPage(Title = "Add new store")
@@ -118,12 +68,30 @@ type AddStorePage(connectionFactory: SQLiteConnectionFactory) as self =
         base.ToolbarItems.Add(save)
         base.Content <- layout
 
+type StoreViewCell() as self =
+    inherit ViewCell()
+
+    let edit =
+        new MenuItem(Text = "Edit",  Icon = FileImageSource.op_Implicit "pencil")
+
+    let delete =
+        new MenuItem(Text = "Delete", Icon = FileImageSource.op_Implicit "bin")
+
+    let layout =
+        new StackLayout()
+        |> StackLayout.AddChild (new Label() |> Label.SetBinding' Label.TextProperty "Name")
+
+    do
+        self.ContextActions.Add(edit)
+        self.ContextActions.Add(delete)
+        
+        self.View <- layout
+
 type StoresPage(connectionFactory: SQLiteConnectionFactory) as self =
     inherit ContentPage(Title = "Stores", Icon = FileImageSource.op_Implicit "shop")
 
     let listView = 
-        new ListView(ItemTemplate = new DataTemplate(typeof<TextCell>))
-        |> ListView.SetTemplateBinding TextCell.TextProperty "Name"
+        new ListView(ItemTemplate = new DataTemplate(typeof<StoreViewCell>))
     
     let add =
         let page =
@@ -149,7 +117,8 @@ type StoresPage(connectionFactory: SQLiteConnectionFactory) as self =
             let store = e.Item :?> Storage.Store
             self.Navigation.PushAsync(new StorePage(store.Id, connectionFactory))
             |> Async.AwaitTask
-            |> Async.StartImmediate)
+            |> Async.StartImmediate
+            listView.SelectedItem <- null)
 
     override self.OnAppearing() =
         refresh()
